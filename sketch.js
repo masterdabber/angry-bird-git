953aabed4841d46bed818b052a2a5a167824b6be
masterdabber <aryapalanivel@gmail.com>
2021-04-27 18:59:39 -0700

    initial report

--- sketch.js
+++ sketch.js
@@ -17,6 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform = new Ground(150,300,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -53,7 +54,7 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
-
+    platform.display();
     box5.display();
     log4.display();
     log5.display();
